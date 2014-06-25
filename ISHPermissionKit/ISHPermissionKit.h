39589739e6b29f08108fa597e371e427212a2972
felixLam <felix@iosphere.de>
2014-06-25 16:42:11 +0200

    Initial Commit

---
+++ ISHPermissionKit/ISHPermissionKit.h
@@ -0,0 +1,19 @@
+//
+//  ISHPermissionKit.h
+//  ISHPermissionKit
+//
+//  Created by Felix Lamouroux on 25.06.14.
+//  Copyright (c) 2014 iosphere GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISHPermissionKit.
+FOUNDATION_EXPORT double ISHPermissionKitVersionNumber;
+
+//! Project version string for ISHPermissionKit.
+FOUNDATION_EXPORT const unsigned char ISHPermissionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISHPermissionKit/PublicHeader.h>
+
+
